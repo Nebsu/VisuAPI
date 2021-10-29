588f5af95e2c995a8106d42d976c3a604c22c237
THEAU Nicolas <theaun91@gmail.com>
2021-10-29 11:34:09 +0200

    Ajouter de la fonction connexion() qui permet de tester la connexion avec le srevur de git et qui imprime une erreur si il y en a une et la quelle La fonction renvoie true si la connexion est valide ou false dans le cas inverse.

--- analyzer/src/main/java/up/visulog/analyzer/getAPI.java
+++ analyzer/src/main/java/up/visulog/analyzer/getAPI.java
@@ -1,20 +1,66 @@
 package up.visulog.analyzer;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class getAPI {
+    private String Token;
+    private String project;
+    
+    public getAPI(String Token) {
+        this.Token = Token;
+    }
+
+    public getAPI() {
+
+    }
 
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://gaufre.informatique.univ-paris-diderot.fr/api/v4/projects/?private_token=8ax_oKvn8CMzvyPmxUD1");
-        InputStream is = url.openConnection().getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        getAPI a = new getAPI("8ax_oKvn8CMzvyPmxUD1");
+        System.out.println(a.connexion());
+        // URL url = new URL("https://gaufre.informatique.univ-paris-diderot.fr/api/v4/groups/1711/members/?private_token=8ax_oKvn8CMzvyPmxUD1");
+        // InputStream is = url.openConnection().getInputStream();
+        // BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        // String line = null;
+        // while ((line = reader.readLine()) != null) {
+        //     System.out.println(line);
+        // }
+        // reader.close();
+    }
+
+    public Map countcommit() { //A FAIRE
+        String s = "https://gaufre.informatique.univ-paris-diderot.fr/api/v4/projects/" + this.project +"/repository/commits";
+        if(this.Token != null) {
+            s = s + "?private_token=" + this.Token;
         }
-        reader.close();
+        else {
+
+        }
+        return new HashMap<>();
+    }
+
+    public boolean connexion() {
+        try {
+            String s = "https://gaufre.informatique.univ-paris-diderot.fr/api/v4/projects";
+            if(this.Token != null) {
+                s = s + "?private_token=" + this.Token;
+            }
+            URL url = new URL(s);
+            InputStream is = url.openConnection().getInputStream();
+        }
+        catch (Exception e) {
+            if(e.toString().equals("java.net.UnknownHostException: gaufre.informatique.univ-paris-diderot.fr")) {
+                System.out.println("Impossible de récupérer la requête.");
+                System.out.println("Vérifiez vos paramètres de connexion.");
+            }
+            else {
+                System.out.println("Clé invalide");
+            }
+            return false;
+        }
+        return true;
     }
 
 }
