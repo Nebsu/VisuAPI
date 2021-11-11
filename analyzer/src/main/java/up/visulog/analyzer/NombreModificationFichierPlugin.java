ffa57894c8733716f100887321c67c94880a2db1
René Tom <re.tom2003@gmail.com>
2021-11-11 16:16:27 +0100

    Tentative ajout modif Fichier parent V1

--- analyzer/src/main/java/up/visulog/analyzer/NombreModificationFichierPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/NombreModificationFichierPlugin.java
@@ -12,6 +12,7 @@ import java.io.*;
 public class NombreModificationFichierPlugin extends getAPI {
     private String Chemin;
     private String Branche;
+    private Set<String> CommitsParcourus = new HashSet<String>();
 
     // L'adresse c'est sans api/v4 et sans l'id projet
     public NombreModificationFichierPlugin(String project, String token, String adresse, String Chemin, String Branche) {
@@ -20,41 +21,80 @@ public class NombreModificationFichierPlugin extends getAPI {
         // Il y a un exemple dans le main
         super(project, token, adresse);
         this.Chemin = Chemin;
-        this.Branche = corrigeHTML(Branche);
+        if (Branche != null) {
+            this.Branche = corrigeHTML(Branche);
+        }
+        else {
+            this.Branche = Branche;
+        }
+        
     }
 
-    public Map<String, Object> NombreModif() throws IOException {
+    public Map<String, Object> NombreModif(String idCommitDepart) throws IOException {
         Map<String, Object> result = new HashMap<String, Object>();
         int acc = 0;
         ArrayList<String[]> L = new ArrayList<String[]>();
-        String idCommit = idLastCommit();
+        String idCommit;
+        if (idCommitDepart == null){
+             idCommit = idLastCommit();
+        }
+        else {
+             idCommit = idCommitDepart;
+        }
         String idParent = "";
         while (true) {
+            System.out.println("Commit ID : " +idCommit);
             idParent = idCommitParent(idCommit);
-            if (idParent.equals("")) {
-                break;
-            }
-            if (idParent.contains(",")) {// Commits a deux parents
-                // // TODO : Add dans la liste L le fait que y'ait fusion
-                String[] tabparent = idParent.split(",");
-                idParent = tabparent[0];
+            if (!CommitsParcourus.contains(idCommit)) {
+                if (idParent.equals("")) {
+                    break;
+                }
+                if (idParent.contains(",")) {// Commits a deux parents
+                    // // TODO : Add dans la liste L le fait que y'ait fusion
+                    String[] tabparent = idParent.split(",");
+                    // System.out.println("Parent 1 : " + tabparent[0]);
+                    // System.out.println();
+                    // System.out.println("Parent 2 : " + tabparent[1]);
+                    // System.out.println();
+                    NombreModificationFichierPlugin temp = new NombreModificationFichierPlugin(this.Project,this.Token,this.Adresse,Chemin,null);
+                    Map<String, Object> temporary = temp.NombreModif(tabparent[1]);
+                    acc+=(int)temporary.get("Nombre");
+                    temporary.remove("Nombre");
+                    result.putAll(temporary);
+                    CommitsParcourus.addAll(temp.CommitsParcourus);
+                    idParent = tabparent[0];
 
-            }
-            if (fichierModif(idCommit)) {
-                acc++;
-                System.out.println("Fichier modifié " + acc + " fois");
-                String[] commits = new String[2];
-                commits[0] = idCommit;
-                commits[1] = idParent;
-                System.out.println(" ID commit :" + commits[0]);
-                System.out.println(" ID parent :" + commits[1]);
-                L.add(commits);
+                }
+                if (fichierModif(idCommit)) {
+                    acc++;
+                    System.out.println("Fichier modifié " + acc + " fois");
+                    String[] commits = new String[2];
+                    commits[0] = idCommit;
+                    commits[1] = idParent;
+                    // System.out.println(" ID commit :" + commits[0]);
+                    // System.out.println(" ID parent :" + commits[1]);
+                    L.add(commits);
+                }
+                CommitsParcourus.add(idCommit);
             }
             idCommit = idParent;
         }
+        Map<String, Object> temp2 = new HashMap<String, Object>();
+        String t = "Commits";
+        int accumulator = 0;
+        while (true) {
+            if (accumulator != 0){
+                t = t.substring(0,7) + accumulator;
+            }
+            System.out.println(t);
+            if(!result.containsKey(t)){
+                break;
+            }
+            accumulator++;
+        }
         result.put("Nombre", acc);
-        result.put("Commits", L);
-        affiche(result);
+        result.put(t, L);
+        result.putAll(temp2);
         return result;
     }
 
@@ -171,4 +211,8 @@ public class NombreModificationFichierPlugin extends getAPI {
         return res;
     }
 
+    public static void main(String[] args) throws IOException{
+        NombreModificationFichierPlugin test = new NombreModificationFichierPlugin("3389","Uje9yszeBg-oGNrpUH9R","https://gaufre.informatique.univ-paris-diderot.fr","README.md","develop");
+        test.affiche(test.NombreModif(null));
+    }
 }
