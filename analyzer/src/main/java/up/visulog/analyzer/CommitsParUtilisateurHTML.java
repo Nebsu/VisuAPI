d82e481a2a28bbabf7a69ac9100e1cb070a51f42
Sebastien Zhou <Sebastien.4084@gmail.com>
2021-11-12 16:48:11 +0100

    V2 CommitsParUtilisateurHTML

--- analyzer/src/main/java/up/visulog/analyzer/CommitsParUtilisateurHTML.java
+++ analyzer/src/main/java/up/visulog/analyzer/CommitsParUtilisateurHTML.java
@@ -1,30 +1,29 @@
 package up.visulog.analyzer;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 public class CommitsParUtilisateurHTML{
-    private int[] nbCommits;
-    private LinkedList<String> members;
+    private Map<String, Object> result;
 
-    public CommitsParUtilisateurHTML(int[]n, LinkedList<String> m){
-        this.nbCommits = n;
-        this.members = m;
+    public CommitsParUtilisateurHTML(Map<String, Object> r){
+        this.result = r;
     }
 
     //Creation du code html
     public String toHTML(){
         String s = "<html><body>";
-        for(int i = 0; i < nbCommits.length; i++){
-            s += "<li>"+members.get(i)+": "+nbCommits[i]+"</li>";
+        for(var item : result.entrySet()){
+            s += "<li>"+item.getKey()+": "+item.getValue()+"</li>";
         }
         return s + "</body></html>";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         CommitsParUtilisateur p = new CommitsParUtilisateur(3389, "bVqyB1SzLYKnSi6u1cdM", 
         "https://gaufre.informatique.univ-paris-diderot.fr");
-        CommitsParUtilisateurHTML c = new CommitsParUtilisateurHTML(p.recupererCommits(), p.recupererMembres());
-        c.toHTML();
+        CommitsParUtilisateurHTML c = new CommitsParUtilisateurHTML(p.recupererCommits());
+        System.out.println(c.toHTML());
     }
 }
