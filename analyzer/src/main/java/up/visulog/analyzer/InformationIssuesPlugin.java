5a417d0ba83b0f5b7b676b42d550798879b2b71f
Ilyas Hamchaoui <ilyas.hamchaoui@etu.u-paris.fr>
2021-11-28 16:54:46 +0100

    Version final du plugin,avec quelques commentairse à ajouter plus tard

--- analyzer/src/main/java/up/visulog/analyzer/InformationIssuesPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/InformationIssuesPlugin.java
@@ -9,6 +9,8 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class InformationIssuesPlugin extends getAPI {
@@ -18,16 +20,16 @@ public class InformationIssuesPlugin extends getAPI {
 
     }
 
-     public LinkedList<String> getTitle(String information) throws IOException, ParseException {
+     public LinkedList<String> getData(String information) throws IOException, ParseException {
             requestIssue("/issues");
             return readAndTabJsonfile(information);
     }
     public LinkedList<String> readAndTabJsonfile(String valeurArecuperer) throws IOException, ParseException {
         LinkedList<String> list = new LinkedList<String>();
         JSONParser jsParser = new JSONParser();
-        JSONArray userArray = (JSONArray) jsParser.parse(new FileReader("request.json"));
-        for (Object user : userArray) {
-            JSONObject temp = (JSONObject) user;
+        JSONArray issueArray = (JSONArray) jsParser.parse(new FileReader("request.json"));
+        for (Object issue : issueArray) {
+            JSONObject temp = (JSONObject) issue;
             list.add(temp.get(valeurArecuperer).toString());
         }
         return list;
@@ -40,14 +42,23 @@ public class InformationIssuesPlugin extends getAPI {
             System.out.println("Erreur dans la récupération de la donnée suivante:" + donnees);
         }
     }
+    public HashMap<String,LinkedList<String>> issuesInfo() throws IOException, ParseException {
+        HashMap<String,LinkedList<String>> data = new HashMap<String,LinkedList<String>>();
+        String [] element = {"iid","title","state","created_at","updated_at","assignees","author"};
+        for (int i = 0; i <element.length ; i++) {
+            data.put(element[i],getData(element[i]));
+            //System.out.println("ça marche" + element[i]);
+        }
+        return data;
+    }
 
     public static void main(String[] args) throws IOException, ParseException {
-        InformationIssuesPlugin p = new InformationIssuesPlugin("3389","MkixHCSQPPj_Pwku9TQ1","https://gaufre.informatique.univ-paris-diderot.fr");
-        LinkedList<String> list = p.getTitle("title");
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        InformationIssuesPlugin p = new InformationIssuesPlugin("3389","65pJShP_Dd8MZPiTGvxC","https://gaufre.informatique.univ-paris-diderot.fr");
+        HashMap<String,LinkedList<String>> h = p.issuesInfo();
+        for (int i = 0; i <h.get("iid").size() ; i++) {
+            System.out.println(h.get("iid").get(i));
+            }
         }
     }
 
-}
 
