33390b2c25e050f65a0f3e3b9bcdf4fce741cab9
Ilyas Hamchaoui <ilyas.hamchaoui@etu.u-paris.fr>
2021-12-03 15:50:36 +0100

    Correction du problème lié au fait qu'il n'y avait pas toute les données

--- analyzer/src/main/java/up/visulog/analyzer/InformationIssuesPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/InformationIssuesPlugin.java
@@ -1,64 +1,87 @@
 package up.visulog.analyzer;
 
-import netscape.javascript.JSObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+
 
 public class InformationIssuesPlugin extends getAPI {
-
+    private HashMap<String, LinkedList<String>> data = new HashMap<String, LinkedList<String>>();
     public InformationIssuesPlugin(String project, String token, String adresse) {
         super(project, token, adresse);
 
     }
 
-     public LinkedList<String> getData(String information) throws IOException, ParseException {
-            requestIssue("/issues");
-            return readAndTabJsonfile(information);
-    }
-    public LinkedList<String> readAndTabJsonfile(String valeurArecuperer) throws IOException, ParseException {
-        LinkedList<String> list = new LinkedList<String>();
+
+
+    public void readAndTabJsonfile(String valeurArecuperer) throws IOException, ParseException {
+
         JSONParser jsParser = new JSONParser();
         JSONArray issueArray = (JSONArray) jsParser.parse(new FileReader("request.json"));
         for (Object issue : issueArray) {
             JSONObject temp = (JSONObject) issue;
-            list.add(temp.get(valeurArecuperer).toString());
+            data.get(valeurArecuperer).add(temp.get(valeurArecuperer).toString());
         }
-        return list;
     }
 
-    public void requestIssue(String donnees) {
-        try {
-            request("projects/" + String.valueOf(this.Project) + donnees, null);
-        } catch (Exception e) {
-            System.out.println("Erreur dans la récupération de la donnée suivante:" + donnees);
+    public void requestIssue() throws IOException, ParseException {
+        int page = 1;
+        int size = 0;
+        int nbrIssues = nbrIssues();
+        initElement();
+        while (size%100 == 0) {
+            try {
+                request("projects/" + String.valueOf(this.Project) + "/issues", "per_page=100&page=" + (page));
+                addList();
+                JSONParser jsonP = new JSONParser();
+                JSONArray issues = (JSONArray) jsonP.parse(new FileReader("request.json"));
+                size += issues.size();
+            } catch (Exception e) {
+                System.out.println("Erreur dans la récupération de la donnée suivante:");
+            }
+            page++;
         }
-    }
-    public HashMap<String,LinkedList<String>> issuesInfo() throws IOException, ParseException {
-        HashMap<String,LinkedList<String>> data = new HashMap<String,LinkedList<String>>();
-        String [] element = {"iid","title","state","created_at","updated_at","assignees","author"};
-        for (int i = 0; i <element.length ; i++) {
-            data.put(element[i],getData(element[i]));
-            //System.out.println("ça marche" + element[i]);
-        }
-        return data;
     }
 
-    public static void main(String[] args) throws IOException, ParseException {
-        InformationIssuesPlugin p = new InformationIssuesPlugin("3389","65pJShP_Dd8MZPiTGvxC","https://gaufre.informatique.univ-paris-diderot.fr");
-        HashMap<String,LinkedList<String>> h = p.issuesInfo();
-        for (int i = 0; i <h.get("iid").size() ; i++) {
-            System.out.println(h.get("iid").get(i));
+    public int nbrIssues() throws IOException, ParseException {
+        request("projects/" + String.valueOf(this.Project) + "/issues_statistics", null);
+        JSONParser jsonP = new JSONParser();
+        JSONObject object = (JSONObject) jsonP.parse(new FileReader("request.json"));
+        JSONObject satistics = (JSONObject) object.get("statistics");
+        JSONObject counts = (JSONObject) satistics.get("counts");
+        Long a = (Long) counts.get("all");
+
+        return Math.toIntExact(a);
+    }
+
+    public void initElement() throws IOException, ParseException {
+        String[] element = {"iid", "title", "state", "created_at", "updated_at", "assignees", "author","description"};
+        for (int i = 0; i < element.length; i++) {
+            data.put(element[i], new LinkedList<String>());
+        }
+    }
+        public void addList() throws IOException, ParseException {
+            String[] element = {"iid", "title", "state", "created_at", "updated_at", "assignees", "author"};
+            for (int i = 0; i < element.length; i++) {
+                readAndTabJsonfile(element[i]);
             }
         }
+
+    public static void main(String[] args) throws IOException, ParseException {
+        InformationIssuesPlugin p = new InformationIssuesPlugin("10582521", "", "https://gitlab.com");
+        p.requestIssue();
+        for (int i = 0; i < p.data.get("iid").size(); i++) {
+           System.out.println(p.data.get("iid").get(i));
+       }
+
     }
+}
 
 
