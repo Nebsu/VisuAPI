c5bc2a6395a74c383b19c7a3d6e7f922cf70bf78
Sebastien Zhou <Sebastien.4084@gmail.com>
2021-12-10 14:41:15 +0100

    Changement emplacement Commit.java -> Classe interne de de CommitHistory.java

--- analyzer/src/main/java/up/visulog/analyzer/CommitHistory.java
+++ analyzer/src/main/java/up/visulog/analyzer/CommitHistory.java
@@ -11,6 +11,25 @@ public class CommitHistory extends getAPI {
 
     private ArrayList<Commit> commitsList;
 
+    class Commit {
+    
+        protected final String title;
+        protected final String date;
+        protected final String author;
+
+        public Commit(String title, String date, String author) {
+            this.title = title;
+            this.date = date;
+            this.author = author;
+        }
+
+        @Override
+        public String toString() {
+            return (this.date + " | " + this.author + "\n: " + this.title);
+        }
+
+    }
+
     // Constructeur :
     public CommitHistory(String project, String token, String adresse) 
     throws IOException {
